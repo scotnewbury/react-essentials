0f57b63d0d4225f6e5228c438352add85d5abcb5
Scot Newbury <scot@scotnewbury.com>
2020-12-12 22:35:55 -0500

    Unordered list via React.createElement

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   React.createElement(
-    "h1", 
-    {style: {color: "blue"}},
-     "Hey Everyone!"
+    "ul",
+    null,
+    React.createElement("li", null, "Monday"),
+    React.createElement("li", null, "Tuesday"),
+    React.createElement("li", null, "Wednesday"),
+    React.createElement("li", null, "Thursday"),
   ),
+
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
