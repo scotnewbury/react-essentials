9fefd65d95c147b656585e7e0a45a838f804aab5
Scot Newbury <scot@scotnewbury.com>
2020-12-12 22:48:32 -0500

    First components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from "./App";
 
 ReactDOM.render(
-  React.createElement(
-    "ul",
-    null,
-    React.createElement("li", null, "Monday"),
-    React.createElement("li", null, "Tuesday"),
-    React.createElement("li", null, "Wednesday"),
-    React.createElement("li", null, "Thursday"),
-  ),
-
+  <App />,
   document.getElementById('root')
 );
