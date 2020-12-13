3a753b6dba9b1ea27add67d711b8bbe11824b58d
Scot Newbury <scot@scotnewbury.com>
2020-12-12 23:02:05 -0500

    Rendering a list from an array

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
 import './App.css';
 
-function Header() {
+function Header(props) {
   return (
     <header>
-      <h1>Eve's Kitchen</h1>
+      <h1>{props.name}'s Kitchen</h1>
     </header>
   )
 }
 
-function Main() {
+function Main(props) {
   return (
     <section>
-      <p>We serve the most delicious food around.</p>
+      <p>We serve the most {props.adjective} food around.</p>
+      <ul style={{textAlign: "left"}}>
+        {props.dishes.map((dish) => (<li>{dish}</li>))}
+      </ul>
     </section>
   )
 }
 
-function Footer() {
+function Footer(props) {
   return (
     <section>
-      <p>It's true.</p>
+      <p>Copyright {props.year}</p>
     </section>
   )
 }
+
+const dishes = [
+  "Macaroni and Cheese",
+  "Salmon and broccoli",
+  "Steak and potatoes"
+]
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+      <Header name="Horacio"/>
+      <Main adjective="amazing" dishes={dishes}/>
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
