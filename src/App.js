f5f3c07b2f61de16c4f3de97598bec51d9307fd1
Scot Newbury <scot@scotnewbury.com>
2020-12-13 18:43:55 -0500

    Checkbox with feedback

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import './App.css';
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondary] = useState("tired");
-
-  useEffect(() => {
-    console.log(`It's ${emotion} around here.`);
-  }, [emotion]);
-
-  useEffect(() => {
-    console.log(`It's ${secondary} around here.`);
-  }, [secondary]);
+  const [checked, setChecked] = useState(false);
 
   return (
     <>
-      <h1>Current emotion is {emotion} and {secondary}.</h1>
-      <button onClick={() => setEmotion("happy")}>Make Happy</button>
-      <button onClick={() => setSecondary("crabby")}>Make Crabby</button>
-      <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
-      
+      <input 
+        type="checkbox" 
+        value={checked} 
+        onChange={() => setChecked(checked => !checked)}
+      />
+      <p>{checked ? "The box is checked" : "The box is not checked"}</p>
     </>
   );
 }
