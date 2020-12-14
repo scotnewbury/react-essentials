818c34b803e1556341d4446039c25f59b1183164
Scot Newbury <scot@scotnewbury.com>
2020-12-14 00:28:00 -0500

    First test using the test-library from react

--- src/App.js
+++ src/App.js
@@ -3,33 +3,13 @@ import './App.css';
 
 // https://api.github.com/users/scotnewbury
 
-function App({login}) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  
-  useEffect(() => {
-    if(!login) return;
-    setLoading(true);
-    fetch(`https://api.github.com/users/${login}`)
-      .then(response => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, [login]);
-
-  if(loading) return <h1>Loading...</h1>;
-  if(error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
-  if(!data) return null;
+function App() {
 
   return (
     <div>
-      <h1>{data.name}</h1>
-      <p>{data.location}</p>
-      <img alt={data.login} src={data.avatar_url} height="200" />
+      <h1>Hello React Testing Library</h1>
     </div>
   )
 
 }
-
 export default App;
